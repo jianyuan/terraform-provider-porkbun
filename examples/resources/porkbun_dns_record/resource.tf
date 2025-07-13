e2f484797cac1a0f98409eed211c47f52091d1d8
vakintosh <vakintosh@protonmail.com>
2025-07-13 01:30:13 -0600

    feat: Add ImportState function

--- examples/resources/porkbun_dns_record/resource.tf
+++ examples/resources/porkbun_dns_record/resource.tf
@@ -27,3 +27,16 @@ resource "porkbun_dns_record" "example" {
   type      = "CNAME"
   content   = "pixie.porkbun.com"
 }
+
+resource "porkbun_dns_record" "example" {
+  domain = "jiancodes.com"
+
+  subdomain = "*"
+  type      = "CNAME"
+  content   = "pixie.porkbun.com"
+}
+
+import {
+  to = porkbun_dns_record.example
+  id  = "123456789_jiancodes.com_CNAME"
+}
